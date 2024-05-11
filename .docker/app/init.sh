8c644e73e88b4ae8d1d55060fd78f06195a2f3de
abrantedevops <abrante.devops@gmail.com>
2024-05-10 23:31:09 -0300

    adjust information details

--- .docker/app/init.sh
+++ .docker/app/init.sh
@@ -40,8 +40,67 @@ echo 'server {
         fastcgi_split_path_info ^(.+\.php)(/.*)$;
         include fastcgi_params;
         fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+        fastcgi_intercept_errors on;
     }
+
+    error_page 403 /403.html;
+    location = /403.html {
+        root /var/www/html/glpi/public;
+        internal;
+    }
+
 }' > /etc/nginx/sites-available/default
+
+if [ ! -f /var/www/html/glpi/public/403.html ]; then
+    echo "<!DOCTYPE html>
+<html lang=\"pt\">
+<head>
+    <meta charset=\"UTF-8\">
+    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+    <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css\">
+    <title>403 Forbidden</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            text-align: center;
+            padding: 50px;
+        }
+
+        h1 {
+            color: #333;
+        }
+
+        p {
+            color: #666;
+            margin-bottom: 20px;
+        }
+
+        .btn {
+            display: inline-block;
+            padding: 10px 20px;
+            background-color: #007bff;
+            color: #fff;
+            text-decoration: none;
+            border-radius: 5px;
+            transition: background-color 0.3s;
+        }
+
+        .btn:hover {
+            background-color: #0056b3;
+        }
+    </style>
+</head>
+<body>
+    <i class=\"fas fa-exclamation-triangle fa-5x\"></i>
+    <h1>Desculpe, mas esta página não existe...</h1>
+    <p>Pedimos desculpas pelo inconveniente, mas a página que você estava tentando acessar não existe neste endereço.</p>
+    <p>Você pode voltar para a página inicial clicando no botão abaixo:</p>
+    <a href="index.php" class=\"btn\"><i class=\"fas fa-home\"></i> Ir para a Página Principal</a>
+</body>
+</html>" > /var/www/html/glpi/public/403.html
+fi
 
 if [ ! -f /etc/nginx/sites-enabled/default ]; then
     ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
