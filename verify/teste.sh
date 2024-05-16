ec70307c0746fd662174a29bd04fecbac3814327
abrantedevops <abrante.devops@gmail.com>
2024-05-15 22:09:11 -0300

    update ci

---
+++ verify/teste.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Check container status
+container_status=$(docker inspect -f '{{.State.Status}}' glpi)
+if [ $? -eq 0 ]; then
+  echo -e "\033[92mContainer status: $container_status (OK)\033[0m"
+else
+  echo -e "\033[91mContainer status: $container_status (Falha)\033[0m"
+  exit 1
+fi
+
+# Check NGINX status in container
+nginx_status=$(docker exec glpi service nginx status)
+if [ $? -eq 0 ]; then
+  echo -e "\033[92mNGINX status: $nginx_status (OK)\033[0m"
+else
+  echo -e "\033[91mNGINX status: $nginx_status (Falha)\033[0m"
+  exit 1
+fi
+
+# Check PHP-FPM v8.3 status in container
+php_status=$(docker exec glpi service php8.3-fpm status)
+if [ $? -eq 0 ]; then
+  echo -e "\033[92mPHP-FPM v8.3 status: $php_status (OK)\033[0m"
+else
+  echo -e "\033[91mPHP-FPM v8.3 status: $php_status (Falha)\033[0m"
+  exit 1
+fi
